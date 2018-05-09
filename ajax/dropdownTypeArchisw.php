14a424ba820f53c95f7252463a2ead1188ce873d
Eric Feron <eric_feron@carrefour.com>
2018-05-09 16:29:42 +0200

    Initial load

---
+++ ajax/dropdownTypeArchisw.php
@@ -0,0 +1,65 @@
+<?php
+/*
+ * @version $Id: HEADER 15930 2011-10-30 15:47:55Z tsmr $
+ -------------------------------------------------------------------------
+ archisw plugin for GLPI
+ Copyright (C) 2009-2016 by the archisw Development Team.
+
+ https://github.com/InfotelGLPI/archisw
+ -------------------------------------------------------------------------
+
+ LICENSE
+      
+ This file is part of archisw.
+
+ archisw is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ archisw is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with archisw. If not, see <http://www.gnu.org/licenses/>.
+ --------------------------------------------------------------------------
+ */
+
+if (strpos($_SERVER['PHP_SELF'],"dropdownTypeArchisw.php")) {
+   $AJAX_INCLUDE=1;
+   include ('../../../inc/includes.php');
+   header("Content-Type: text/html; charset=UTF-8");
+   Html::header_nocache();
+}
+
+Session::checkCentralAccess();
+
+// Make a select box
+if (isset($_POST["swcomponenttype"])) {
+   $used = array();
+
+   // Clean used array
+   if (isset($_POST['used']) && is_array($_POST['used']) && (count($_POST['used']) > 0)) {
+      $query = "SELECT `id`
+                FROM `glpi_plugin_archisw_swcomponents`
+                WHERE `id` IN (".implode(',',$_POST['used']).")
+                      AND `plugin_archisw_swcomponenttypes_id` = '".$_POST["swcomponenttype"]."'";
+
+      foreach ($DB->request($query) AS $data) {
+         $used[$data['id']] = $data['id'];
+      }
+   }
+
+   Dropdown::show('PluginArchiswSwcomponent',
+                  array('name'      => $_POST['myname'],
+                        'used'      => $used,
+                        'width'     => '50%',
+                        'entity'    => $_POST['entity'],
+                        'rand'      => $_POST['rand'],
+                        'condition' => "glpi_plugin_archisw_swcomponents.plugin_archisw_swcomponenttypes_id='".$_POST["swcomponenttype"]."'"));
+
+}
+
+?>
