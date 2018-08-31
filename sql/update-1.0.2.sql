217c8f2f04ce812419aab6242176d2aaa770718f
Eric Feron <ericferon@scarlet.be>
2018-08-31 13:37:35 +0200

    update-1.0.2.sql : suppressed id by insert

--- sql/update-1.0.2.sql
+++ sql/update-1.0.2.sql
@@ -1,1 +1,1 @@
-INSERT INTO glpi_plugin_archisw_swcomponents_itemroles (id,itemtype,name,comment) VALUES (6,'PluginArchiswSwcomponent','(To be/Being) Replaced by','Which application will replace this one ?');
+INSERT INTO glpi_plugin_archisw_swcomponents_itemroles (itemtype,name,comment) VALUES ('PluginArchiswSwcomponent','(To be/Being) Replaced by','Which application will replace this one ?');
